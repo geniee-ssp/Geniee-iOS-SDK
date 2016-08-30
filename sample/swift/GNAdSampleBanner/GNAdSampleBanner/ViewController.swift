2a89d54653b16894c27ab7206b935f409a671948
k-koh <k.koh@geniee.co.jp>
2016-08-30 23:04:19 +0900

    update sample app for xcode8,swift2.3

--- sample/swift/GNAdSampleBanner/GNAdSampleBanner/ViewController.swift
+++ sample/swift/GNAdSampleBanner/GNAdSampleBanner/ViewController.swift
@@ -7,7 +7,7 @@ import UIKit
 
 class ViewController: UIViewController, GNAdViewDelegate {
     
-    var _adView : GNAdView = GNAdView(frame: CGRectMake(0, 20, 320, 50),
+    var _adView : GNAdView = GNAdView(frame: CGRect(x: 0, y: 20, width: 320, height: 50),
                                  adSizeType:GNAdSizeTypeSmall,
                                       appID:"YOUR_SSP_APP_ID")
 
@@ -18,8 +18,8 @@ class ViewController: UIViewController, GNAdViewDelegate {
         _adView.rootViewController = self
         //_adView.bgColor = YES
         //_adView.geoLocationEnable = YES
-        //_adView.GNAdlogPriority = GNLogPriorityInfo
-        _adView.center = CGPointMake(self.view.center.x, _adView.center.y);
+        //_adView.gnAdlogPriority = GNLogPriorityInfo
+        _adView.center = CGPoint(x: self.view.center.x, y: _adView.center.y);
         
         self.view.addSubview(_adView)
         _adView.startAdLoop()
--- sample/swift/GNAdSampleMultipleBanner/Models/MyCellData.swift
+++ sample/swift/GNAdSampleMultipleBanner/Models/MyCellData.swift
@@ -11,7 +11,7 @@ class MyCellData: NSObject {
     var title: NSString
     var content: NSString
     override init() {
-        let random:Int32 = rand() % 2
+        let random:UInt32 = arc4random() % 2
         if (random == 0) {
             imgURL = NSURL(string: "http://media.gssp.asia/img/bf0/8f2/bf08f28d19c98e2b603a21519a0948f6.png")!
             title = "title"
