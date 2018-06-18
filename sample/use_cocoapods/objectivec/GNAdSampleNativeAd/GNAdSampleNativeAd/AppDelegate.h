ab6c73b6bdbedbc270d6ae67fc72a31d0759fb14
k-tomoyasu <k.tomoyasu@geniee.co.jp>
2018-06-18 18:10:55 +0900

    add native sample use cocoapods

---
+++ sample/use_cocoapods/objectivec/GNAdSampleNativeAd/GNAdSampleNativeAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GNAdSampleNativeAd
+//
+//  Created by Tomoyasu Kouta on 2018/06/18.
+//  Copyright © 2018年 Geniee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/use_cocoapods/objectivec/GNAdSampleNativeAd/GNAdSampleNativeAd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GNAdSampleNativeAd
+//
+//  Created by Tomoyasu Kouta on 2018/06/18.
+//  Copyright © 2018年 Geniee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
