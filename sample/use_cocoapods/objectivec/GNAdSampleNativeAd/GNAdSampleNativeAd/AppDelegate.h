589727b1153a1d2ec1e0a3448822413df4168f4e
k-tomoyasu <k.tomoyasu@geniee.co.jp>
2018-06-21 15:58:39 +0900

    add native sample xcodeproj

--- sample/use_cocoapods/objectivec/GNAdSampleNativeAd/GNAdSampleNativeAd/AppDelegate.h
+++ sample/use_cocoapods/objectivec/GNAdSampleNativeAd/GNAdSampleNativeAd/AppDelegate.h
@@ -2,9 +2,6 @@
 //  AppDelegate.h
 //  GNAdSampleNativeAd
 //
-//  Created by Tomoyasu Kouta on 2018/06/18.
-//  Copyright © 2018年 Geniee. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- sample/use_cocoapods/objectivec/GNAdSampleNativeAd/GNAdSampleNativeAd/main.m
+++ sample/use_cocoapods/objectivec/GNAdSampleNativeAd/GNAdSampleNativeAd/main.m
@@ -2,9 +2,6 @@
 //  main.m
 //  GNAdSampleNativeAd
 //
-//  Created by Tomoyasu Kouta on 2018/06/18.
-//  Copyright © 2018年 Geniee. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
